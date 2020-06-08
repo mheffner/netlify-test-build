7b8c4e1f7cc4b4844461cbac2de0a63164e2f8ba
Mike Heffner <mikeh@fesnel.com>
2020-06-08 17:04:01 -0400

    test big file

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Hello from build land!"
+echo "Hello from big-file build cache test!"
+
+# dump out a big file, must be random to prevent compression
+dd if=/dev/urandom of=big-file.data bs=1M count=15360
+
+mv big-file.data $HOME/.cache/
